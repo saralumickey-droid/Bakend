841dc3d2fb282933d93637be72573f04a255e4fa
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-26 07:50:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -323,5 +323,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
